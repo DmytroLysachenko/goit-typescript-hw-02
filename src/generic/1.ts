2fdab2d3bf77716083576ab6b9989caf96956adf
Dimka863 <dlysachenko98@gmail.com>
2024-05-27 08:34:38 +0200

    done

--- src/generic/1.ts
+++ src/generic/1.ts
@@ -3,7 +3,7 @@
   Доповніть цю функцію, використовуючи generics, щоб вона повертала правильний тип.
 */
 
-function getPromise() {
+function getPromise(): Promise<Array<string | number>> {
   const promise: Promise<Array<string | number>> = new Promise((resolve) => {
     resolve(['Text', 50]);
   });
